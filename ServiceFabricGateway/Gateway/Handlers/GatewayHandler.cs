4623c2aed7ef2da5731699a8cdd47248b78121c4
Mike Gore <mikegore1000@msn.com>
2017-04-09 09:18:13 +0100

    Initial commit

---
+++ ServiceFabricGateway/Gateway/Handlers/GatewayHandler.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Services.Client;
+using Newtonsoft.Json.Linq;
+
+namespace Gateway.Handlers
+{
+    // TODO: Transient retries
+    public class GatewayHandler : DelegatingHandler
+    {
+        private readonly HttpClient client;
+
+        private readonly List<GatewayRoute> routes = new List<GatewayRoute>
+        {
+            new GatewayRoute("/finance/test", "fabric:/ServiceFabricSpiking/TestApi")
+        };
+
+        public GatewayHandler(HttpClient client)
+        {
+            this.client = client;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            if (request.RequestUri.Segments.Length < 3)
+            {
+                return new HttpResponseMessage(HttpStatusCode.NotFound);
+            }
+
+            var routeKey = $"{request.RequestUri.Segments[0]}{request.RequestUri.Segments[1]}{request.RequestUri.Segments[2]}";
+            var matchedRoute = routes.SingleOrDefault(r => r.Matches(routeKey));
+            var routePostfix = string.Concat(request.RequestUri.Segments.Skip(3));
+
+            if (matchedRoute == null)
+            {
+                return new HttpResponseMessage(HttpStatusCode.NotFound);
+            }
+
+            var serviceUri = await GetAddress(matchedRoute, routePostfix, cancellationToken);
+            var response = await ProxyRequest(serviceUri, request);
+
+            return response;
+        }
+
+        private Task<HttpResponseMessage> ProxyRequest(Uri serviceUri, HttpRequestMessage request)
+        {
+            var proxiedRequest = request.Clone(serviceUri);
+
+            return client.SendAsync(proxiedRequest);
+        }
+
+        private async Task<Uri> GetAddress(GatewayRoute matchedRoute, string routePostfix, CancellationToken cancellationToken)
+        {
+            var resolver = ServicePartitionResolver.GetDefault();
+            var resolved = await resolver.ResolveAsync(
+                new Uri(matchedRoute.FabricAddress),
+                new ServicePartitionKey(),
+                cancellationToken
+            );
+
+            JObject addresses = JObject.Parse(resolved.GetEndpoint().Address);
+            var baseUri = new Uri((string) addresses["Endpoints"].First());
+            return new Uri(baseUri, $"{matchedRoute.RouteKey}/{routePostfix}");
+        }
+    }
+}
---
+++ ServiceFabricGateway/Gateway/Startup.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Http;
+using System.Web.Http;
+using Gateway.Handlers;
+using Owin;
+
+namespace Gateway
+{
+    public static class Startup
+    {
+        // This code configures Web API. The Startup class is specified as a type
+        // parameter in the WebApp.Start method.
+        public static void ConfigureApp(IAppBuilder appBuilder)
+        {
+            var client = CreateClient();
+
+            // Configure Web API for self-host. 
+            HttpConfiguration config = new HttpConfiguration();
+
+            config.MessageHandlers.Add(new GatewayHandler(client));
+
+            appBuilder.UseWebApi(config);
+        }
+
+        private static HttpClient CreateClient()
+        {
+            // TODO: Take into account this http://byterot.blogspot.co.uk/2016/07/singleton-httpclient-dns.html
+
+            return new HttpClient();
+        }
+    }
+}
